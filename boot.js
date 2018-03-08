5e5c986389dfdc9f6cec193eecac9a1ae759d88f
sean <326771876@qq.com>
2018-03-08 18:06:39 +0800

    branch test

---
+++ boot.js
@@ -0,0 +1,26 @@
+__CreateJSPath = function (js) {
+    var scripts = document.getElementsByTagName("script");
+    var path = "";
+    for (var i = 0, l = scripts.length; i < l; i++) {
+        var src = scripts[i].src;
+        if (src.indexOf(js) != -1) {
+            var ss = src.split(js);
+            path = ss[0];
+            break;
+        }
+    }
+    var href = location.href;
+    href = href.split("#")[0];
+    href = href.split("?")[0];
+    var ss = href.split("/");
+    ss.length = ss.length - 1;
+    href = ss.join("/");
+    if (path.indexOf("https:") == -1 && path.indexOf("http:") == -1 && path.indexOf("file:") == -1 && path.indexOf("\/") != 0) {
+        path = href + "/" + path;
+    }
+    return path;
+};
+
+var bootPATH = __CreateJSPath("boot.js");
+document.write('<script src="' + bootPATH + 'jquery-2.1.4.js" type="text/javascript"></script>');
+document.write('<script src="' + bootPATH + 'common.js" type="text/javascript"></script>');
