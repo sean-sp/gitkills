ff62790bf8af032104bf7b02f676bd7aa45d4d2f
sean <326771876@qq.com>
2018-03-09 17:10:52 +0800

    add consoleLogIssue

--- boot.js
+++ boot.js
@@ -20,8 +20,7 @@ __CreateJSPath = function (js) {
     }
     return path;
 };
-console.log('dev');
-console.log('dev1');
+console.log('issue-101');
 var bootPATH = __CreateJSPath("boot.js");
 document.write('<script src="' + bootPATH + 'jquery-2.1.4.js" type="text/javascript"></script>');
 document.write('<script src="' + bootPATH + 'common.js" type="text/javascript"></script>');
